2cb31a6313d74e95e1716952963eca8595f4c6ef
mixtera <abdukadirovashurmuhammad@gmail.com>
2020-10-09 23:10:11 +0500

    first commit

---
+++ app/src/main/java/com/mixtera/dicee/MainActivity.java
@@ -0,0 +1,58 @@
+package com.mixtera.dicee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final   ImageView leftdice;
+        leftdice= (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightdice;
+        rightdice = (ImageView) findViewById(R.id.image_rightDice);
+
+       final int [] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Dicee", "the button has been pressed");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee", "The random number is:" +number );
+
+                leftdice.setImageResource(diceArray[number]);
+
+                number= randomNumberGenerator.nextInt(6);
+                rightdice.setImageResource(diceArray[number]);
+
+            }
+        });
+
+
+
+    }
+}
